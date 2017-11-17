c4697e88a985bf4f724d137418bde52c01555585
Chris Joe <chris@apptimize.com>
2017-11-17 14:45:38 -0800

    Fix indentation/formatting in deps.sh

--- deps.sh
+++ deps.sh
@@ -10,10 +10,10 @@ source $SCRIPTPATH/config.sh
 
 if [ "$MOS" == "OSX" ]; then
 
-# disabling fink!
-# some problems with sierra + general unhappiness with slowness/flakiness
-# stand by as we migrate away
-# source $SCRIPTPATH/os/osx/fink.sh
+    # disabling fink!
+    # some problems with sierra + general unhappiness with slowness/flakiness
+    # stand by as we migrate away
+    # source $SCRIPTPATH/os/osx/fink.sh
 
     # install some qa dependencies, see RET-1466
     if [ ! -e /usr/local/bin/brew ]; then
@@ -35,24 +35,24 @@ if [ "$MOS" == "OSX" ]; then
     $BREW uninstall --force carthage &> /dev/null || true
     $BREW install carthage
 
-# put our own cafile in place
-sudo cp $VENV/lib/python2.7/site-packages/certifi/cacert.pem $($VENV/bin/python -c 'import ssl;print ssl.get_default_verify_paths().openssl_cafile')
+    # put our own cafile in place
+    sudo cp $VENV/lib/python2.7/site-packages/certifi/cacert.pem $($VENV/bin/python -c 'import ssl;print ssl.get_default_verify_paths().openssl_cafile')
 
 elif [ "$MOS" == "Ubuntu" ]; then
 
-source $SCRIPTPATH/os/ubuntu/apt.sh
+    source $SCRIPTPATH/os/ubuntu/apt.sh
 
-sudo bash -c "echo $VENV/lib > /etc/ld.so.conf.d/venv.conf"
-sudo ldconfig
+    sudo bash -c "echo $VENV/lib > /etc/ld.so.conf.d/venv.conf"
+    sudo ldconfig
 
 elif [ "$MOS" == "Arch" ]; then
 
-source $SCRIPTPATH/os/arch/pacman.sh
+    source $SCRIPTPATH/os/arch/pacman.sh
 
-sudo bash -c "echo $VENV/lib > /etc/ld.so.conf.d/venv.conf"
-sudo ldconfig
+    sudo bash -c "echo $VENV/lib > /etc/ld.so.conf.d/venv.conf"
+    sudo ldconfig
 
 else
-echo "Error -- unsupported platform"
-exit 1
+    echo "Error -- unsupported platform"
+    exit 1
 fi
