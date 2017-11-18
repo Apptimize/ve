c3d10f335b253e8000234c45d7347b02537c1df3
Chris Joe <chris@apptimize.com>
2017-11-17 16:35:07 -0800

    RET-1984: idevice* binaries are out of date in /ave/bin RET-1986: ave does not put Homebrew in $PATH
    Make sure we have homebrew accessible in the path, and the latest versions of our required packages are installed.

--- deps.sh
+++ deps.sh
@@ -15,25 +15,13 @@ if [ "$MOS" == "OSX" ]; then
     # stand by as we migrate away
     # source $SCRIPTPATH/os/osx/fink.sh
 
-    # install some qa dependencies, see RET-1466
+    # install homebrew
     if [ ! -e /usr/local/bin/brew ]; then
         echo "homebrew not found, installing homebrew"
         # install homebrew using macOS system ruby
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     fi
-    BREW="/usr/local/bin/brew"
-    # ideviceinstaller only works for ios 9. for ios 10, carthage is installed below
-    $BREW uninstall --force ideviceinstaller &> /dev/null || true
-    $BREW install ideviceinstaller
-    # install ios-deploy using brew, not npm
-    $BREW uninstall --force ios-deploy &> /dev/null || true
-    $BREW install ios-deploy
-    # install from HEAD to get important updates
-    $BREW uninstall --force libimobiledevice &> /dev/null || true
-    $BREW install libimobiledevice
-    # install carthage
-    $BREW uninstall --force carthage &> /dev/null || true
-    $BREW install carthage
+
 
     # put our own cafile in place
     sudo cp $VENV/lib/python2.7/site-packages/certifi/cacert.pem $($VENV/bin/python -c 'import ssl;print ssl.get_default_verify_paths().openssl_cafile')
--- pkgs/zz-test-automation.sh
+++ pkgs/zz-test-automation.sh
@@ -9,6 +9,29 @@ else
     mv chromedriver $VENV/bin/
 fi
 
+# brew requirements
+BREW="/usr/local/bin/brew"
+# Uninstall all old versions to make sure our new versions install cleanly
+# because they might be interdependent
+$BREW uninstall --force ideviceinstaller &> /dev/null || true
+$BREW uninstall --force ios-deploy &> /dev/null || true
+$BREW uninstall --force libimobiledevice &> /dev/null || true
+$BREW uninstall --force carthage &> /dev/null || true
+# NOTE: Homebrew doesn't let you install specific versions
+# install carthage
+$BREW install carthage
+# install from HEAD to get important updates
+$BREW install --HEAD libimobiledevice
+# clean up any previous idevice* binaries
+rm $VENV/bin/idevice* &> /dev/null || true
+# ideviceinstaller only works for ios 9. for ios 10+, we use carthage
+$BREW install ideviceinstaller
+# install ios-deploy using brew, not npm
+$BREW install ios-deploy
+
+# prevent libimobiledevice errors
+sudo chmod -R 777 /var/db/lockdown
+
 # appium requirements
 $VENV/bin/npm uninstall -g appium
 $VENV/bin/npm install -g appium@1.7.1  # this does not install cleanly on arch...
