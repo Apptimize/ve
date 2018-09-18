cb662e8afeb9b799836f939cea2924495758d610
Yinmeng Zhang <yinmeng.zhang@gmail.com>
2018-09-17 17:07:41 -0700

    Fix java home on mac

--- pkgs/available/scripts.sh
+++ pkgs/available/scripts.sh
@@ -76,7 +76,7 @@ export PATH
 
 if [ -e "/usr/libexec/java_home" ]; then
 # this works on osx...
-JAVA_HOME="\$(/usr/libexec/java_home -v '1.7*')"
+JAVA_HOME="\$(/usr/libexec/java_home -v '1.8*')"
 export JAVA_HOME
 fi
 
